43bef71f5af06add5d5d0b9d6f4c9327ea9de1cc
DarkCWK <1224702603@qq.com>
2022-09-06 21:37:26 +0800

    添加语种 英语,日语,韩语,法语,德语,俄语,西班牙语,葡萄牙语,意大利,越南语,印尼语,阿拉伯语,荷兰语,泰语

--- src/YoudaoTranslate.ts
+++ src/YoudaoTranslate.ts
@@ -1,4 +1,5 @@
 import { ITranslate, ITranslateOptions } from "comment-translate-manager";
+import { createHash } from "node:crypto";
 import HttpUtils from "./HttpUtils";
 
 type Result = {
@@ -10,20 +11,110 @@ type Result = {
 
 export default class YoudaoTranslate implements ITranslate {
   maxLen: number = 3000;
+
+  transformLanguage(origin: string) {
+    switch (origin) {
+      case "en": // 英语
+      case "ja": // 日语
+      case "ko": // 韩语
+      case "fr": // 法语
+      case "de": // 德语
+      case "ru": // 俄语
+      case "es": // 西班牙语
+      case "pt": // 葡萄牙语
+      case "it": // 意大利
+      case "vi": // 越南语
+      case "id": // 印尼语
+      case "ar": // 阿拉伯语
+      case "nl": // 荷兰语
+      case "th": // 泰语
+        return origin;
+      case "zh-CN": // 简体中文
+        return "zh-CHS";
+      default:
+        return undefined;
+    }
+  }
+
   translate(content: string, options: ITranslateOptions): Promise<string> {
     return new Promise(async (resolve, reject) => {
-      let body = `doctype=json&type=AUTO&i=${content}`;
-      let headers = { "Content-Type": "application/x-www-form-urlencoded" };
-      let data = await HttpUtils.post("http://fanyi.youdao.com/translate", body, headers);
-      let resultJson: Result = JSON.parse(data.toString("utf8"));
+      console.log(options.from, options.to);
 
-      let str = content;
-      for (const _ of resultJson.translateResult) {
-        for (const __ of _) {
-          str = str.replace(__.src, __.tgt);
-        }
+      let from = options.from ? this.transformLanguage(options.from) : "AUTO";
+      let to = options.to ? this.transformLanguage(options.to) : undefined;
+
+      if (to == undefined) {
+        return reject("未支持此语言");
       }
-      resolve(str);
+
+      let ua =
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.27";
+
+      let i = content;
+
+      let lts = String(Date.now());
+
+      let salt = lts + Math.floor(10 * Math.random());
+
+      let signMd5 = createHash("md5");
+      let sign = signMd5.update("fanyideskweb" + i + salt + "Ygy_4c=r#e#4EX^NUGUc5").digest("hex");
+
+      let bvMd5 = createHash("md5");
+      let bv = bvMd5.update(ua.substring(ua.indexOf("/") + 1)).digest("hex");
+
+      let body =
+        `i=${i}` +
+        `&` +
+        `from=${from}` +
+        `&` +
+        `to=${to}` +
+        `&` +
+        `smartresult=dict` +
+        `&` +
+        `client=fanyideskweb` +
+        `&` +
+        `salt=${salt}` +
+        `&` +
+        `sign=${sign}` +
+        `&` +
+        `lts=${lts}` +
+        `&` +
+        `bv=${bv}` +
+        `&` +
+        `doctype=json` +
+        `&` +
+        `version=2.1` +
+        `&` +
+        `keyfrom=fanyi.web` +
+        `&` +
+        `action=FY_BY_REALTlME`;
+
+      let headers = {
+        "User-Agent": ua,
+        Referer: "https://fanyi.youdao.com/",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Cookie:
+          "OUTFOX_SEARCH_USER_ID=-1432640551@10.110.96.154; OUTFOX_SEARCH_USER_ID_NCOO=1848060309.009117; SESSION_FROM_COOKIE=unknown; ___rl__test__cookies=1662464966342",
+      };
+
+      HttpUtils.post("http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule", body, headers).then(
+        (v) => {
+          let result = JSON.parse(v.toString("utf-8"));
+          if (result.errorCode == 0) {
+            let tgt = "";
+            for (const translateRowResult of result.translateResult) {
+              for (const translateResult of translateRowResult) {
+                tgt += translateResult.tgt;
+              }
+              tgt += "\n";
+            }
+
+            console.log(tgt);
+
+            resolve(tgt);
+          }
+        }
+      );
     });
   }
   link(content: string, options: ITranslateOptions): string {
